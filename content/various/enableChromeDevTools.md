bafc586db888a43219a2612870c30c0f38436e63
akshaysin <akshaysin@gmail.com>
2018-04-22 16:39:48 -0400

    init

---
+++ content/various/enableChromeDevTools.md
@@ -0,0 +1,34 @@
+Title: Enabling Developer tools and set Chrome as default browser in Windows
+Date: 2018-03-18 11:30 AM
+Modified: 2018-03-18 11:30 AM
+Category: Various
+Tags: chrome, devtools
+Slug: enableChromeDevTools
+Authors: Akshay Sinha
+Summary: Enabling Developer tools and set Chrome as default browser in Windows
+
+It used to bug me that every time I opened chrome, Developer tools was disabled by default and I had to edit the registry to get it enabled. To gratify my lazy bones, I put together a short power shell script to update the registry for me. Please feel free to share it.
+
+        if ( Get-ItemProperty HKLM:\SOFTWARE\Policies\Google\Chrome) {   
+                Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Name DeveloperToolsDisabled -Value 0  
+                write-host "Devtools enabled successfully" -foregroundcolor green  
+                Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Name DefaultBrowserSettingEnabled -Value 0  
+                write-host "Chrome set as default browser" -foregroundcolor green
+        }
+        else {  
+                write-host "HKLM:\SOFTWARE\Policies\Google\Chrome path not found !!" -foregroundcolor red
+        }
+
+Just save it somewhere on your box with an extension of .ps1 and execute it when needed as given below :
+
+        powershell -file myfile.ps1
+
+Note, If using powershell for the first time on your box, you might get an error like :
+
+        .... cannot be loaded because the execution of scripts is disabled on this system.
+
+To bypass this, as an Administrator, you can set the execution policy by typing this into your PowerShell window:
+
+        Set-ExecutionPolicy RemoteSigned
+
+However make sure to understand the full implications of that [setting](https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-powershell-1.0/ee176961(v=technet.10)) before executing the above command
---
+++ pelicanconf.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Akshay Sinha'
+SITENAME = 'An Average Joe'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'English'
+
+''' In addition to below MARKDOWN settings, add following block to end of index.html
+
+        <script>
+           $(document).ready(function () {
+              $("table").attr("class","table table-condensed table-bordered");
+           });
+        </script>
+
+ '''
+
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.tables':{},
+    }
+}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PLUGIN_PATHS = ['pelican-plugins', ]
+# PLUGINS = ['i18n_subsites','disqus_static', ]
+PLUGINS = ['i18n_subsites',]
+JINJA_ENVIRONMENT = {
+    'extensions': ['jinja2.ext.i18n'],
+}
+
+# Blogroll
+LINKS = (('Resume', 'docs/Resume.pdf'),
+         ('Email','https://spamty.eu/mail/v4/830/2RWUyWci8Q2f55cff1/'))
+
+# Social widget
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/akshay-sinha-91215954'),
+          ('github', 'https://github.com/akshaysin'))
+
+# MENUITEMS = (
+#     ('Email', 'https://spamty.eu/mail/v4/830/2RWUyWci8Q2f55cff1/'),
+#     ('Resume', 'docs/Resume.pdf')
+#     )
+
+DEFAULT_PAGINATION = 10
+
+BOOTSTRAP_FLUID=False
+BOOTSTRAP_NAVBAR_INVERSE=True
+# BANNER='images/download.jpg'
+# BANNER_SUBTITLE=''
+PYGMENTS_STYLE='solarizeddark'
+DISPLAY_ARTICLE_INFO_ON_INDEX=True
+ABOUT_ME='A confused coder'
+AVATAR='images/akshay.jpg'
+DISPLAY_CATEGORIES_ON_SIDEBAR=False
+DISPLAY_ARCHIVE_ON_SIDEBAR=True
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+SHARIFF = False
+
+'''add following to the end of index.html to enable addThis analytics
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5adbce4fc80f08db"></script>
+'''
+
+ADDTHIS_PROFILE = 'ra-5adbce4fc80f08db'
+
+# for disque comments
+
+# PLUGINS = [u"disqus_static"]
+
+DISQUS_SITENAME = 'https-akshaysin-github-io'
+# DISQUS_SECRET_KEY = 'jLI9aGvlRANtJ5Rqy4MWQL0aaKgKKvqQ06nR26iwY3ORczKrgLvMcswHJjRm8KRD'
+# DISQUS_PUBLIC_KEY = 'KTXp0mcORVKrX0GY6PpdmFhQhSM67gxcpxA0igehWyTdwpELZnju3zsTQaDNb1pz'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
